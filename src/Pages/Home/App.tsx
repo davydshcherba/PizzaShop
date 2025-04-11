2534c4719056e059dd5cf54d3102c31dae5abf54
davydshcherba <davidserba302@gmail.com>
2025-04-11 23:26:34 +0300

    refactor(UI): improve layout and styling for better readability and consistency
    - Adjusted Aside component height for better visual balance
    - Restructured App component to include a main content section
    - Updated color scheme and typography for improved contrast and aesthetics

--- src/Pages/Home/App.tsx
+++ src/Pages/Home/App.tsx
@@ -2,11 +2,13 @@ import './App.css'
 import Aside from "../../Components/Aside/Aside";
 
 function App() {
-
   return (
-    <>
+    <div className="app-container">
       <Aside />
-    </>
+      <main>
+        <h1>Hello</h1>
+      </main>
+    </div>
   )
 }
 
