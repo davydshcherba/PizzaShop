7eba34a17ef5379abb09bec3897b2fe321d81fc3
Eloy527 <davidserba302@gmail.com>
2025-04-11 19:10:49 +0300

    feat: add profile page with layout and routing
    Introduce a new profile page with a responsive layout and routing. The page includes a container with a sidebar and main content area, styled using CSS. The profile page is accessible via the `/profile` route.

---
+++ src/Pages/Profile/Profile.tsx
@@ -0,0 +1,21 @@
+import Aside from '../../Components/Aside/Aside'
+import './Profile.css'
+
+function Profile() {
+    return (
+        <div className="profile-container">
+            <Aside />
+            <div className="profile-main">
+                <div className="profile_data">
+                    <h1>Hello John Smith!</h1>
+                    <div className="profile__left">
+                        <h4>@johnsmith</h4>
+                        <h3>Joined on 12/01/2021</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import App from "./Pages/Home/App.tsx";
 import "./index.css"
 import NotFound from "./Pages/NotFound/NotFound..tsx";
+import Profile from "./Pages/Profile/Profile.tsx";
 
 const root = document.getElementById("root");
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(root!).render(
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<App />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="*" element={<NotFound />} />
         </Routes>
     </BrowserRouter>
